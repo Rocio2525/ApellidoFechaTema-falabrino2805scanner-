04e26e352a8e9088cc297bce09db81354e0ea5d5
Rocio2525 <71531844+Rocio2525@users.noreply.github.com>
2020-11-25 19:27:33 -0300

    Add files via upload

---
+++ Usuario.java
@@ -0,0 +1,34 @@
+
+package usuario;
+import javax.swing.JOptionPane;
+
+public class Usuario {
+
+    public static void main(String[] args) {
+      String usuario="rocio";
+     String  contraseña="R1234";
+    
+      
+      
+      //datos usuario
+     
+       usuario = JOptionPane.showInputDialog("Ingrese el nombre de Usuario:  ");
+        contraseña = JOptionPane.showInputDialog("Ingrese la contraseña " + usuario + " :  ");
+        
+        
+        if(usuario.equals(usuario) && contraseña.equals(contraseña)){
+            JOptionPane.showMessageDialog(null,"se ingresa al sistema " + usuario );
+            }else {
+            JOptionPane.showMessageDialog(null,"Usuario y/o contraseña no es correcta","Error credenciales",JOptionPane.ERROR_MESSAGE);
+        
+        } 
+        
+      } 
+
+          
+      
+    }
+    
+    
+    
+
